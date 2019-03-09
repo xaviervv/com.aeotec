af09aedf22ab59ad4ab392f4b35db95304ff24a2
Matthew Waanders <matthew@athom.nl>
2019-03-09 11:06:11 +0100

    Fixes Door/Window Sensor 6 not reporting contact alarm

--- drivers/ZW112/device.js
+++ drivers/ZW112/device.js
@@ -5,7 +5,7 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class ZW112 extends ZwaveDevice {
 
 	onMeshInit() {
-		this.registerCapability('alarm_contact', 'NOTIFICATION');
+		this.registerCapability('alarm_contact', 'BASIC');
 		this.registerCapability('measure_batttery', 'BATTERY');
 	}
 
