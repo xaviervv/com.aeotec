4e47e164e71f65c91cfec86855f030ed78b33d7a
Matthew Waanders <matthew@athom.nl>
2019-04-29 13:14:32 +0200

    Fixed motion for Multisensor 4-in-1

--- drivers/DSB05/device.js
+++ drivers/DSB05/device.js
@@ -5,12 +5,13 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class AeotecFourInOneDevice extends ZwaveDevice {
 
 	onMeshInit() {
-		this.registerCapability('measure_battery', 'BATTERY');
-		this.registerCapability('alarm_motion', 'BASIC', {
-		    reportParser: report => report['Value'] === 255,
-            reportParserOverride: true
+        this.registerReportListener('BASIC', 'BASIC_SET', (report) => {
+            this.log('ReportListener');
+            this.log(report);
+            this.setCapabilityValue('alarm_motion', report['Value'] === 255);
         });
 
+		this.registerCapability('measure_battery', 'BATTERY');
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
         this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL');
         this.registerCapability('measure_humidity', 'SENSOR_MULTILEVEL');
