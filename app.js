287fab163a94de74dddcfd6d9315def29f3f06be
Emile Nijssen <emile@athom.nl>
2020-05-07 11:31:53 +0200

    cleanup

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const Homey = require('homey');
 class AeotecApp extends Homey.App {
 	onInit() {
 		this.log(`${Homey.manifest.id} running...`);
-		let rainbowAction = new Homey.FlowCardAction('zw098_rainbow')
-            .register()
-            .registerRunListener((args, state) => {
-            	return args.device.rainbowModeHandler(args);
-			});
 	}
 }
 
---
+++ drivers/ZW098/driver.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Homey = require('homey');
+
+class AeotecLEDBulb5Driver extends Homey.Driver {
+    onInit() {
+        super.onInit();
+
+        let rainbowAction = new Homey.FlowCardAction('zw098_rainbow')
+          .register()
+          .registerRunListener((args, state) => {
+          	return args.device.rainbowModeHandler(args);
+    			});
+    }
+}
+
+module.exports = AeotecLEDBulb5Driver;
